4f9246a8aa6ab75ba0c8eeb161f190f799969da8
Nicole Nagatoshi <nmnag@hawaii.edu>
2019-11-15 21:11:48 -0800

    changes

---
+++ Nagatoshi_Nicole_Assignment1/server.js
@@ -0,0 +1,16 @@
+var express = require('express');
+const querystring = require('querystring'); // requiring a query string - string of whatever is written in textbox
+const product_data = require('./public/product_data.js'); //using data from product_data.js
+
+var app = express();
+
+app.get('/invoice.html', function(req, res, next) {
+    if(typeof req.query['purchase_submit'] != 'undefined') {
+        console.log(Date.now() + ': Purchase made from ip ' + req.ip + ' data: ' + JSON.stringify(req.query));
+    }
+    next();
+});
+
+app.use(express.static('./static'));
+
+var listener = app.listen(8080, () => { console.log('server started listening on port ' + listener.address().port) });
